78d7805a8d82586305fdf82e1cd7524bda920ad0
Dor Oren <doroe@mta.ac.il>
2021-05-13 15:26:24 +0300

    Add project files.

---
+++ Ex03.GarageLogic/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    abstract class Vehicle
+    {
+        public string Model { get; }
+        public string RegistrationNumber { get; }
+        public float EnergyPercentage { get; }
+
+    }
+}
